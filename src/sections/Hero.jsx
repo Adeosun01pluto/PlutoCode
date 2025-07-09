8ac1ac9e3ab9f0a1dca172b8f5a680dfe94be6cd
maria01steve <maria01steve@gmail.com>
2025-07-08 18:47:37 -0700

    added tap to scroll down

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,92 @@
+// import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
+
+// import AnimatedCounter from "../components/AnimatedCounter";
+// import Button from "../components/Button";
+// import { words } from "../constants";
+// import HeroExperience from "../components/models/hero_models/HeroExperience";
+
+// const Hero = () => {
+//   useGSAP(() => {
+//     gsap.fromTo(
+//       ".hero-text h1",
+//       { y: 50, opacity: 0 },
+//       { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" }
+//     );
+//   });
+
+//   return (
+//     <section id="hero" className="relative overflow-hidden">
+//       <div className="absolute top-0 left-0 z-10">
+//         <img src="/images/bg.png" alt="" />
+//       </div>
+
+//       <div className="hero-layout">
+//         {/* LEFT: Hero Content */}
+//         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+//           <div className="flex flex-col gap-7">
+//             <div className="hero-text">
+//               <h1>
+//                 Shaping
+//                 <span className="slide">
+//                   <span className="wrapper">
+//                     {words.map((word, index) => (
+//                       <span
+//                         key={index}
+//                         className="flex items-center md:gap-3 gap-1 pb-2"
+//                       >
+//                         <img
+//                           src={word.imgPath}
+//                           alt="person"
+//                           className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+//                         />
+//                         <span>{word.text}</span>
+//                       </span>
+//                     ))}
+//                   </span>
+//                 </span>
+//               </h1>
+//               <h1>into Real Projects</h1>
+//               <h1>that Deliver Results</h1>
+//             </div>
+
+//             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+//               Hi, I’m Adeosun, I build stunning user interfaces and web applications
+//             </p>
+
+//             <Button
+//               text="See My Work"
+//               className="md:w-80 md:h-16 w-60 h-12"
+//               id="counter"
+//             />
+//         <div className='z-[1000] w-full flex justify-center items-center'>
+//           <a href='#counter'>
+//             <div className='w-[35px] h-[52px] rounded-3xl border-4 border-gray-100/60 flex justify-center items-start p-2'>
+//               <div
+//                 className='w-3 h-3 rounded-full bg-white-50 mb-1'
+//               />
+//             </div>
+//           </a>
+//         </div>
+//           </div>
+//         </header>
+
+
+//         {/* RIGHT: 3D Model or Visual */}
+//         <figure>
+//           <div className="hero-3d-layout">
+//             <HeroExperience />
+//           </div>
+//         </figure>
+//       </div>
+
+//       <AnimatedCounter />
+//     </section>
+//   );
+// };
+
+// export default Hero;
+
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
@@ -51,7 +140,8 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Adeosun, I build stunning user interfaces and web applications
+              Hi, I’m Adeosun, I build stunning user interfaces and web
+              applications
             </p>
 
             <Button
@@ -59,18 +149,21 @@ const Hero = () => {
               className="md:w-80 md:h-16 w-60 h-12"
               id="counter"
             />
-        <div className='z-[1000] w-full flex justify-center items-center'>
-          <a href='#counter'>
-            <div className='w-[35px] h-[52px] rounded-3xl border-4 border-gray-100/60 flex justify-center items-start p-2'>
-              <div
-                className='w-3 h-3 rounded-full bg-white-50 mb-1'
-              />
+            <div className="z-[1000] w-full flex flex-col justify-center items-center mt-2 sm:mt-[-50px]">
+              <a href="#counter">
+                <div className="w-[35px] h-[52px] rounded-3xl border-4 border-gray-100/60 flex justify-center items-start p-2">
+                  {/* Applied the bounce animation here */}
+                  <div
+                    className="w-3 h-3 rounded-full bg-white-50"
+                  />
+                </div>
+              </a>
+              <p className="text-white-50 text-sm mt-2"
+              style={{ animation: 'bounce-scroll 1.5s infinite' }} 
+              >Tap to scroll down</p>
             </div>
-          </a>
-        </div>
           </div>
         </header>
-
 
         {/* RIGHT: 3D Model or Visual */}
         <figure>
